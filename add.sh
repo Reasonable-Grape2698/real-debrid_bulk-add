d3a9ca644da9b9499d70421b3db57eddc66bbe9e
Reasonable-Grape2698 <dyl.secure@gmail.com>
2025-01-03 09:59:14 +1100

    Update add.sh

--- add.sh
+++ add.sh
@@ -10,10 +10,34 @@ done
 headers="Authorization: Bearer $a"
 baseurl="https://api.real-debrid.com/rest/1.0/torrents"
 
+function show_progress {
+    current="$1"
+    total="$2"
+
+    # calculate the progress in percentage
+    percent=$(bc <<< "scale=$bar_percentage_scale; 100 * $current / $total" )
+    # The number of done and todo characters
+    done=$(bc <<< "scale=0; $bar_size * $percent / 100" )
+    todo=$(bc <<< "scale=0; $bar_size - $done" )
+
+    # build the done and todo sub-bars
+    done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
+    todo_sub_bar=$(printf "%${todo}s" | tr " " "${bar_char_todo}")
+
+    # output the bar
+    echo -ne "\rProgress : [${done_sub_bar}${todo_sub_bar}] ${percent}%"
+
+    if [ $total -eq $current ]; then
+        echo -e "\nDONE"
+    fi
+}
+
+task_in_total=$(ls -l $dir/*.torrent | wc -l)
+
 for file in $dir/*.torrent; do
         response=$(curl -s --request PUT -H "Content-Type: application/octet-stream" -H "$headers" --data-binary @"$file" "https://api.real-debrid.com/rest/1.0/torrents/addTorrent")
         torrentId=$(echo $response | jq .id)
-
+        count=$(echo "$count+1" | bc)
         # If there was an error
         if [ $torrentId == "null" ]
         then
@@ -28,5 +52,6 @@ for file in $dir/*.torrent; do
                 echo $torrentId
                 mv "$file" $dir/processed
         fi
+        show_progress $count $task_in_total
         sleep 1
-done
+done < for file in $dir/*.torrent; do echo $file; done
