b87c17dd43f4afe3993e8550306a9b4ae79a1e1b
Reasonable-Grape2698 <dyl.secure@gmail.com>
2025-01-03 13:23:31 +1100

    Update select.sh

--- select.sh
+++ select.sh
@@ -8,39 +8,21 @@ done
 headers="Authorization: Bearer $a"
 baseurl="https://api.real-debrid.com/rest/1.0/"
 
-function show_progress {
-    current="$1"
-    total="$2"
-
-    # calculate the progress in percentage
-    percent=$(bc <<< "scale=$bar_percentage_scale; 100 * $current / $total" )
-    # The number of done and todo characters
-    done=$(bc <<< "scale=0; $bar_size * $percent / 100" )
-    todo=$(bc <<< "scale=0; $bar_size - $done" )
-
-    # build the done and todo sub-bars
-    done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
-    todo_sub_bar=$(printf "%${todo}s" | tr " " "${bar_char_todo}")
-
-    # output the bar
-    echo -ne "\rProgress : [${done_sub_bar}${todo_sub_bar}] ${percent}%"
-
-    if [ $total -eq $current ]; then
-        echo -e "\nDONE"
-    fi
-}
 task_in_total=$(cat $f | wc -l)
+count=0
 
 while read $torrentId; do
         response=$(curl -s -X POST -H "$headers" -d "files=1" $baseurl/selectFiles/$torrentId)
         count=$(echo "$count+1" | bc)
         if [ -n $(echo $response | jq .error) ]
         then
-                echo $torrentId
+                echo "$count/$task_in_total"
+                sed -i '1d' $f
+                $torrentId > completed.txt
+                sleep 0.25
         else
                 echo $response
+                $torrentId > failed.txt
                 sleep 60
         fi
-        show_progress $count $task_in_total
-        sleep 0.25
-done < "$f"
+done < $f
