5b641f1f8063a33e6fbb8a2f62451ceb57a7bc4f
Reasonable-Grape2698 <dyl.secure@gmail.com>
2025-01-06 08:55:53 +1100

    Error handling

--- select.sh
+++ select.sh
@@ -8,12 +8,14 @@ done
 headers="Authorization: Bearer $a"
 baseurl="https://api.real-debrid.com/rest/1.0"
 
-task_in_total=$(cat $f | wc -l)
+task_in_total=$(cat "$dir/torrentIds.txt" | wc -l)
 count=0
-f = "$dir/torrentIds.txt"
+
+touch "$dir/activeLim.txt"
+touch "$dir/failed.txt"
 
 # Remove quotation marks
-sed -i 's/\"//g' $f
+sed -i 's/\"//g' "$dir/torrentIds.txt"
 
 while read -r torrentId
 do
@@ -23,16 +25,25 @@ do
         if [ -z $(echo $response | jq .error) ]
         then
                 echo "$count/$task_in_total"
-                sed -i '1d' $f
+                sed -i '1d' "$dir/torrentIds.txt"
                 echo $torrentId >> $dir/completed.txt
                 echo $response
                 sleep 1
         else
-                echo $response
-                echo $torrentId >> $dir/failed.txt
                 if [ $(echo $response | jq .error_code) == "34" ]
                 then
                         sleep 60
+                        echo "Sleeping 60, API limit hit"
+                fi
+                if [ $(echo $response | jq .error_code) == "21" ]
+                then
+                        echo $torrentId >> $dir/activeLim.txt
+                        sed -i '1d' "$dir/torrentIds.txt"
+                        echo "Active limit hit, output to activeLim.txt"
+                else
+                        echo $response
+                        echo $torrentId >> $dir/failed.txt
+                        sed -i '1d' "$dir/torrentIds.txt"
                 fi
         fi
-done < "$f"
+done < "$dir/torrentIds.txt"
